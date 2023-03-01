594b8c4b7519306ecef1109c9119fff901ee2411
taeke2 <taeke2@naver.com>
2023-03-01 23:19:52 +0900

    회원 서비스 개발

---
+++ src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,45 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);  // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // 같은 이름이 있는 중복 회원X
+        /*Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> { // result 값이 있으면
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });*/
+        // 위, 아래 같은 동작임
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
